715730d30fc39e55538763f83548c2ca256bd4af
Yuriy Novikov <tow1987@yandex.ru>
2022-09-03 15:15:19 +0300

    Create new project with Spring Security (test)

---
+++ src/test/java/com/nov/springsecuritytestapp/SpringSecurityTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nov.springsecuritytestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
