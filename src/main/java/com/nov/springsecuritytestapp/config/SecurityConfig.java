32d8f371ade1336eb31918b6461e2245d1c13ab3
Yuriy Novikov <tow1987@yandex.ru>
2022-09-04 16:04:03 +0300

    Remove own Authentication Provider

--- src/main/java/com/nov/springsecuritytestapp/config/SecurityConfig.java
+++ src/main/java/com/nov/springsecuritytestapp/config/SecurityConfig.java
@@ -1,25 +1,34 @@
 package com.nov.springsecuritytestapp.config;
 
 
-import com.nov.springsecuritytestapp.security.AuthProviderImpl;
+import com.nov.springsecuritytestapp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 //Класс устарел, в дальнейшем заменить
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) {
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // Этот метод настраивает аутентификацию, надо юзать свою логику authenticationProvider
-        auth.authenticationProvider(authProvider);
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        // Не шифрованный пароль - deprecated
+        return NoOpPasswordEncoder.getInstance();
     }
 }
